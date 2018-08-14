0024f267ddbe4e0d9c541d206cdfb121a231d66b
TRUNG Q HOANG <trunghoang@harviton.com>
2018-08-14 08:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string) {
 }
 
 
-function sayHiToGrandma(uppercase) {
-  if (uppercase.toUpperCase() === uppercase) return "YES INDEED!";
-
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) return "YES INDEED!";
+  if (string.toLowerCase() === string) return "I can\'t hear you!";
+  if (string === "I love you, Grandma.") return "I love you, too.";
 }
