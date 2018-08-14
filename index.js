bbeeb4912b421e7353091d8fd78799e88d796985
TRUNG Q HOANG <trunghoang@harviton.com>
2018-08-14 07:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
 
   string.toUpperCase() === string return "YES INDEED!"
-  string.toLowerCase() === string return "I can\'t hear you!"
-  string === "I love you, Grandma." return "I love you, too."
   
 }
